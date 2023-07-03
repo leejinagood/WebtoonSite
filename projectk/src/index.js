e559a34209600f82c19392a9d43f3c8daefde42f
solbin <126735005+solbinsol@users.noreply.github.com>
2023-07-03 17:58:58 +0900

    page

--- projectk/src/index.js
+++ projectk/src/index.js
@@ -4,7 +4,9 @@ import './index.css';
 import App from './App';
 import MainPage from './page/MainPage';
 import reportWebVitals from './reportWebVitals';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
+serviceWorkerRegistration.register();
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
--- projectk/src/page/MainPage.jsx
+++ projectk/src/page/MainPage.jsx
@@ -1,10 +1,108 @@
 import React from "react";
+import MainPageCss from "../styles/MainPageCss.css";
 
 const MainPage = () =>{
 
     return(
-       <div className="header">
-            <h1>Avati</h1>
+        <div className="MainPage">
+            <div className="header">
+                    <h1 className="Logo">Avatye Webtoon</h1>
+                    <input className="SerchBar" type="text"  value="작가/제목으로 검색할 수 있습니다."/>
+                    <button className="LoginBtn">로그인</button>
+            </div>
+            <div className="DayBox">
+                <div className="Day">
+                <li>요일 전체</li>
+                    <li>월</li>
+                    <li>화</li>
+                    <li>수</li>
+                    <li>목</li>
+                    <li>금</li>
+                    <li>토</li>
+                    <li>일</li>
+
+                </div>
+            </div>
+            <h3 className="Categories">이달의 신규 웹툰</h3>
+            <div className="NewToon">
+
+                <div className="NewToonInfo">
+                    <img src="1.jpg"></img>
+                    <span className="Title">제목</span>
+                    <p className="Info">작품 설명</p>
+                </div>
+                <div className="NewToonInfo">
+                    <img src="1.jpg"></img>
+                    <span className="Title">제목</span>
+                    <p className="Info">작품 설명</p>
+                </div>
+                <div className="NewToonInfo">
+                    <img src="1.jpg"></img>
+                    <span className="Title">제목</span>
+                    <p className="Info">작품 설명</p>
+                </div>
+            </div>
+            <h3 className="Categories">요일별 전체 웹툰</h3>
+            <div className="AllToon">
+                <li>
+                <div className="Monday">
+                    <div className="AllToonInfo">
+                        <img src="1.jpg"></img>
+                        <p className="Title">제목</p>
+                    </div>
+                    <div className="AllToonInfo">
+                        <img src="1.jpg"></img>
+                        <p className="Title">제목</p>
+                    </div>
+                    <div className="AllToonInfo">
+                        <img src="1.jpg"></img>
+                        <p className="Title">제목</p>
+                    </div>
+                    <div className="AllToonInfo">
+                        <img src="1.jpg"></img>
+                        <p className="Title">제목</p>
+                    </div>
+                    <div className="AllToonInfo">
+                        <img src="1.jpg"></img>
+                        <p className="Title">제목</p>
+                    </div>
+                    <div className="AllToonInfo">
+                        <img src="1.jpg"></img>
+                        <p className="Title">제목</p>
+                    </div>
+                </div>
+                </li>
+                <li>
+                <div className="Tuesday">
+                    <div className="AllToonInfo">
+                        <img src="1.jpg"></img>
+                        <p className="Title">제목</p>
+                    </div>
+                    <div className="AllToonInfo">
+                        <img src="1.jpg"></img>
+                        <p className="Title">제목</p>
+                    </div>
+                    <div className="AllToonInfo">
+                        <img src="1.jpg"></img>
+                        <p className="Title">제목</p>
+                    </div>
+                    <div className="AllToonInfo">
+                        <img src="1.jpg"></img>
+                        <p className="Title">제목</p>
+                    </div>
+                    <div className="AllToonInfo">
+                        <img src="1.jpg"></img>
+                        <p className="Title">제목</p>
+                    </div>
+                    <div className="AllToonInfo">
+                        <img src="1.jpg"></img>
+                        <p className="Title">제목</p>
+                    </div>
+                </div>
+                </li>
+            </div>
        </div>
     )
 }
+
+export default MainPage;
