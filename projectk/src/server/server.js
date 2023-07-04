92c7a17614445a13f0463623a0fcc10a461fc366
리진아 <mnb2098@naver.com>
2023-07-04 18:15:26 +0900

    express에서 restify로 수정한 후 api 생성

--- projectk/src/server/server.js
+++ projectk/src/server/server.js
@@ -1,13 +1,24 @@
-const { error } = require('console');
-const express = require('express');
+// sudo npm install restify --save
+// npm install mysql2
+// npm install nodejs
+// npm install express
 
-const app = express();
-//기본 포트번호 설정
+// const { error } = require('console');
+// const express = require('express');
+const restify = require('restify');
+
+//서버 설정 및 미들웨어
+const server = restify.createServer();
+server.use(restify.plugins.bodyParser());// JSON 데이터 파싱을 위한 미들웨어 등록
+
+// //기본 포트번호 설정
+// const app = express();
 const port = 3000;
 
 //미들웨어
-app.use(express.json());
-app.use(express.urlencoded({extends: true}));
+// app.use(express.json());
+// app.use(express.urlencoded({extends: true}));
+
 
 const getConn = async () => {
     return await pool.getConnection(async(conn) => conn) ;
@@ -20,18 +31,22 @@ const pool = mysql.createPool({
     port : '3306',
     user : 'root',
     password : '2098',
-    database : 'project'
+    database : 'webtoon'
 });
 
 //http://localhost:3000/ 접속
-app.listen(port, ()=>{
+server.listen(port, ()=>{
     console.log("페이지 구동 시작");
 });
 
-
-app.get('/monn', async(req, res) => {
+//url에서 요일을 받아와 웹툰 제목을 출력하는 메서드
+server.get('/:day', async (req, res) => {
     const conn = await getConn();
-    const query = 'select webtoon_name from Twebtoondetail where week = "Monday";';
-    let [rows] = await conn.query(query, []);
+    const day = req.params.day;
+    const query = 'select Twebtoon.webtoon_name from Twebtoon join Twebtoondetail on Twebtoon.webtoon_id = Twebtoondetail.webtoon_id where Twebtoondetail.week = ?;';
+    let [rows] = await conn.query(query, [day]);
+  
     res.send(rows);
-});
+  });
+  
+  
