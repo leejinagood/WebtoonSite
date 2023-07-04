9de0691253df3fa0f657de504e8cba6ee1e4c8d7
solbin <126735005+solbinsol@users.noreply.github.com>
2023-07-04 18:41:12 +0900

    웹툰 목록페이지 작성 중

--- projectk/src/page/ListPage.jsx
+++ projectk/src/page/ListPage.jsx
@@ -7,8 +7,11 @@ class ListPage extends Component{
         title:'제목',
         week: '월',
         writer:'작가',
+        episode:1,
         star:9.9,
-        info:'나다라마바사 하 가나다라마 바사\n\n AbcDefG HH'
+        like:0,
+        uproad:'2023.07.04',
+        info:'나다라마바사 하 가나다라마 바사\n AbcDefG HH'
       }
     render(){
     return(
@@ -20,13 +23,47 @@ class ListPage extends Component{
                 </div>
                 <div className="ListInfo" >
                     <h3>{this.props.title}</h3>
-                    <p>글/그림<span>{this.props.writer}</span> | {this.props.week}요웹툰</p>
+                    <p className="GrayP">글/그림<span>{this.props.writer}</span> | {this.props.week}요웹툰</p>
                     <div className="ToonTinfo">
                         <p>{this.props.info}
                         </p>
                     </div>
                 </div>
+                <div className="InfoBtn">
+                    <button id="PointBtn" className="IBtn">
+                        좋아요 {this.props.like}
+                    </button>
+                    <button className="IBtn">
+                        첫화보기 1화
+                    </button>
+                    <button className="SNSBTN">
+                        공유하기
+                    </button>
+                </div>
+                <div className="List">
+                    <li>
+                        <div className="ListItem">
+                            <div className="ListImg">
+                                <img src="1.jpg" alt="s" />
+                            </div>
+                            <div>
+                                <p className="Episode">
+                                    {this.props.episode}화 {this.props.title}
+                                </p>
+                                <p>
+                                    {this.props.star}<span className="tab">{this.props.uproad}</span>
+                                </p>
+                            </div>
+                        </div>
+                    </li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                </div>
             </div>
+            
         </div>
     )
     }
