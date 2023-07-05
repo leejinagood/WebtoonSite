d97b9097106a67fe4a45f1235fa820102978815f
리진아 <mnb2098@naver.com>
2023-07-05 18:03:08 +0900

    db rds로 설정 변경

--- my-app/src/server/server.js
+++ my-app/src/server/server.js
@@ -29,10 +29,10 @@ const getConn = async () => {
 //데이터베이스 연동
 const mysql = require('mysql2/promise');
 const pool = mysql.createPool({
-    host : '127.0.0.1',
+    host : 'webtoon.cq14nnpiflfq.us-east-1.rds.amazonaws.com',
     port : '3306',
-    user : 'root',
-    password : '2098',
+    user : 'admin',
+    password : 'abcd1234',
     database : 'webtoon'
 });
 
