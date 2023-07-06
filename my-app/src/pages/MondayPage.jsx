edba0ad68965cf37e7e4fb066dfc72a1ffd12cab
solbin <126735005+solbinsol@users.noreply.github.com>
2023-07-06 14:38:09 +0900

    pwa화면 조정중

--- my-app/src/pages/MondayPage.jsx
+++ my-app/src/pages/MondayPage.jsx
@@ -9,6 +9,20 @@ import DayToonItem from "../item/DayToonItem";
 import Rank from "../item/Rank";
 
 class MondayPage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      dayToonItemCounts: []
+    };
+  }
+
+  componentDidMount() {
+    // 데이터베이스에서 요일별 아이템 수를 가져오는 비동기 작업 수행
+    // 예시로 고정된 값을 설정
+    const fetchedDayToonItemCounts = [2, 0, 0];
+    this.setState({ dayToonItemCounts: fetchedDayToonItemCounts });
+  }
+
   static defaultProps = {
     title: '제목',
     week: '월',
@@ -17,30 +31,32 @@ class MondayPage extends Component {
   }
 
   render() {
+    const { dayToonItemCounts } = this.state;
+
     return (
-      <div>
+      <div className="DayBox">
         <Header />  
         <h3 className="Categories">{this.props.week}요일 추천 웹툰</h3>
         <div className="NewToon">
           <NewToon />
-
         </div>
         <div className="Mid">
           <div className="DayToon">
             <h3>전체{this.props.week}요 웹툰</h3>
             <table>
-                <tr>
-                  <td>
-                    <DayToonItem />
-                  </td>
-                  <td>
-                    <DayToonItem />
-                  </td>
-                </tr>
+              <tbody>
+                {dayToonItemCounts.map((count, index) => (
+                  <tr key={index}>
+                    {[...Array(count)].map((_, subIndex) => (
+                      <td key={subIndex}>
+                        <DayToonItem />
+                      </td>
+                    ))}
+                  </tr>
+                ))}
+              </tbody>
             </table>
-
           </div>
-
           <Rank />
         </div>
         <Footer />
