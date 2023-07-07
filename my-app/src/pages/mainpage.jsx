99170744fd4ca71274278b3b77d93dc72b919cd5
리진아 <mnb2098@naver.com>
2023-07-07 11:11:25 +0900

    ..

--- my-app/src/pages/mainpage.jsx
+++ my-app/src/pages/mainpage.jsx
@@ -5,10 +5,9 @@ import Footer from "./footer";
 import NewToon from "../item/NewToon";
 import Tag from "../item/Tag";
 import Head from 'next/head';
-import AllToonInfo from "../item/AllToonInfo"; // AllToonInfo 컴포넌트를 import
-
 const MainPage = () => {
   return (
+    
     <div className="MainPage">
       <Head>
         <link rel="manifest" href="/manifest.json" />
@@ -19,35 +18,100 @@ const MainPage = () => {
       </div>
       <h3 className="Categories">요일별 전체 웹툰</h3>
       <div className="AllToon">
+        <div className="AllTonnbox">
         <table>
           <tbody>
             <tr>
               <td>
-                <AllToonInfo /> 
+                <div className="ATBox">
+                <div className="AllToonInfo">
+                  <img src="1.jpg" alt="" />
+                  <p className="ToonTitle">제목</p>
+                </div>
+                </div>
               </td>
               <td>
-                <AllToonInfo /> 
+              <div className="ATBox">
+                <div className="AllToonInfo">
+                  <img src="1.jpg" alt="" />
+                  <p className="ToonTitle">제목</p>
+                </div>
+                </div>
               </td>
               <td>
-                <AllToonInfo /> 
+              <div className="ATBox">
+                <div className="AllToonInfo">
+                  <img src="1.jpg" alt="" />
+                  <p className="ToonTitle">제목</p>
+                </div>
+                </div>
               </td>
-              </tr>
+          </tr>
+          
+            <tr>
               <td>
-                  <AllToonInfo /> 
-                </td>
-                <td>
-                  <AllToonInfo /> 
-                </td>
-                <td>
-                  <AllToonInfo /> 
-                </td>
+              <div className="ATBox">
+                <div className="AllToonInfo">
+                  <img src="1.jpg" alt="" />
+                  <p className="ToonTitle">제목</p>
+                </div>
+                </div>
+              </td>
+              <td>
+              <div className="ATBox">
+                <div className="AllToonInfo">
+                  <img src="1.jpg" alt="" />
+                  <p className="ToonTitle">제목</p>
+                </div>
+                </div>
+              </td>
+              <td>
+              <div className="ATBox">
+                <div className="AllToonInfo">
+                  <img src="1.jpg" alt="" />
+                  <p className="ToonTitle">제목</p>
+                </div>
+                </div>
+              </td>
+            </tr>
+            
+            {/* 여러 개의 <tr> 요소들 추가 */}
           </tbody>
         </table>
+        </div>
       </div>
-      <Tag />
+
+      <Tag/>
       <Footer />
     </div>
   );
 };
 
 export default MainPage;
+
+
+
+// <table>
+//           <tbody>
+//             <tr>
+//               <td>
+//                 <AllToonInfo /> 
+//               </td>
+//               <td>
+//                 <AllToonInfo /> 
+//               </td>
+//               <td>
+//                 <AllToonInfo /> 
+//               </td>
+//               </tr>
+//               <td>
+//                   <AllToonInfo /> 
+//                 </td>
+//                 <td>
+//                   <AllToonInfo /> 
+//                 </td>
+//                 <td>
+//                   <AllToonInfo /> 
+//                 </td>
+//           </tbody>
+//         </table>
