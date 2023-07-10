7e081855f67caeb4d0ba1270425306cdc149112d
리진아 <mnb2098@naver.com>
2023-07-10 17:31:01 +0900

    에러 주석

--- my-app/src/item/AllToonInfo.jsx
+++ my-app/src/item/AllToonInfo.jsx
@@ -1,22 +1,22 @@
 import React, { useEffect, useState } from "react";
 
 const AllToonInfo = () => {
-  const [webtoons, setWebtoons] = useState([]);
+  // const [webtoons, setWebtoons] = useState([]);
 
-  useEffect(() => {
-    fetch("/api/daywebtoon?day=All")
-      .then((response) => response.json())
-      .then((data) => {
-        setWebtoons(data.webtoons);
-      })
-      .catch((error) => {
-        console.error("Error fetching API:", error);
-      });
-  }, []);
+  // useEffect(() => {
+  //   fetch("/api/daywebtoon?day=All")
+  //     .then((response) => response.json())
+  //     .then((data) => {
+  //       setWebtoons(data.webtoons);
+  //     })
+  //     .catch((error) => {
+  //       console.error("Error fetching API:", error);
+  //     });
+  // }, []);
 
   return (
     <div className="ATBox">
-      {webtoons.map((webtoon, index) => (
+      {/* {webtoons.map((webtoon, index) => (
         <div className="AllToonInfo" key={index}>
           <img src="1.jpg" alt="" />
           <p className="ToonTitle">{webtoon}</p>
@@ -24,7 +24,7 @@ const AllToonInfo = () => {
       ))}
       {webtoons.length % 3 !== 0 && (
         <div className="AllToonInfo" style={{ visibility: "hidden" }}></div>
-      )}
+      )} */}
     </div>
   );
 };
