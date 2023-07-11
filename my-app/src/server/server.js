0145ec442e3add2cf6b758d091e96da7e99fd093
리진아 <mnb2098@naver.com>
2023-07-11 17:08:00 +0900

    이미ㅣㅈ

--- my-app/src/server/server.js
+++ my-app/src/server/server.js
@@ -214,3 +214,22 @@ server.get('/api/LoginPage', async (req, res) => {
   }
 });
 
+// var http = require('http');
+// var fs = require('fs');
+
+// var app = http.createServer(function(request, response){
+//     console.log(request.url);
+    
+//     if(request.url ==='/'){
+//         response.writeHead(200);
+//         response.end(fs.readFileSync(__dirname + '\\index.html'));    
+//     }
+//     if(request.url==='/picture'){
+//         fs.readFile('/Users/leejina/Desktop/leejina/WebtoonSite/my-app/src/WebtoonImg/web1/web1_thumbnail.jpg', function(err, data){
+//             console.log('picture loading...');
+//             response.writeHead(200);
+//             response.write(data);
+//             response.end();    
+//         });
+//     }
+// });
