7db35fa0ba3ba847641f51ed845c664cbcaeee78
리진아 <mnb2098@naver.com>
2023-07-11 17:49:21 +0900

    rank 이미지 추가

--- my-app/src/item/Rank.jsx
+++ my-app/src/item/Rank.jsx
@@ -21,6 +21,23 @@ class Rank extends Component {
   }
 
   render() {
+    const getThumbnailImage = (webtoon) => {
+      if (webtoon.webtoon_name === "똑 닮은 딸") {
+        return "/WebtoonImg/web1/web1_thumbnail.jpg";
+      } else if (webtoon.webtoon_name === "마루는 강쥐") {
+        return "/WebtoonImg/web2/web2_thumbnail.jpg";
+      } else if (webtoon.webtoon_name === "소녀재판") {
+        return "/WebtoonImg/web3/web3_thumbnail.jpg";
+      } else if (webtoon.webtoon_name === "신혼일기") {
+          return "/WebtoonImg/web4/web4_thumbnail.jpg";
+      } else if (webtoon.webtoon_name === "외모지상주의") {
+        return "/WebtoonImg/web5/web5_thumbnail.jpg";
+      }else if (webtoon.webtoon_name === "퀘스트지상주의") {
+        return "/WebtoonImg/web6/web6_thumbnail.jpg";
+      }
+      // 기본값으로 설정할 썸네일 이미지 경로
+      return "";
+    };
     const { webtoons } = this.state;
 
     return (
@@ -33,7 +50,7 @@ class Rank extends Component {
                 <div className="Rank">
                   <div className="Rankitem">
                     <div className="RankImg">
-                      <img src="1.jpg" alt={`${index + 1}등`} />
+                    <img src={getThumbnailImage(webtoon)} alt={`${index + 1}등`}  />
                     </div>
                     <div className="RankNum">
                       <h2>{`${index + 1}등`}</h2>
