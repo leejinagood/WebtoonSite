5c0c7c079d1f522224f1eb363aa6e529cde018a3
리진아 <mnb2098@naver.com>
2023-07-12 14:42:00 +0900

    기존 버전으로

--- my-app/src/pages/ListPage/ListItem.jsx
+++ my-app/src/pages/ListPage/ListItem.jsx
@@ -1,32 +1,21 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import Link from 'next/link';
 
-const ListItem = ({ webtoonName, uploadDate  }) => {
-  useEffect(() => {
-    fetch("/api/daywebtoon?day=All")
-      .then((response) => response.json())
-      .then((data) => {
-        setWebtoons(data.webtoons);
-      })
-      .catch((error) => {
-        console.error("Error fetching API:", error);
-      });
-
-      
-  }, []);
-
-  const [webtoons, setWebtoons] = useState([]);
+const ListItem = ({ webtoonName, ep, uploadDate, handleClick }) => {
+  const handleItemClick = () => {
+    handleClick(ep); // ep 값을 업데이트
+  };
 
   return (
-    <Link href="/WebToonPage/WebToonPage">
-      <div className="ListItem">
+    <Link href={`/WebToonPage/${ep}`}>
+      <div className="ListItem" onClick={handleItemClick}>
         <div className="ListImg">
           <img src="1.jpg" alt="s" />
         </div>
         <div className="ListItemContent">
           <p className="Episode">
             {webtoonName} <br />
-            <span className="tab">sss</span>
+            <span className="tab">{ep}화</span>
           </p>
           <p className="SU">
             <span className="tab">{uploadDate}</span>
