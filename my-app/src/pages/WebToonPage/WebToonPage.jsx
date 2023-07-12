aef36afd5131eab33ace37ca81436589309c9221
solbin <126735005+solbinsol@users.noreply.github.com>
2023-07-12 14:33:45 +0900

    Revert "WebToonPage 클릭레이아웃에 회차 적용"
    This reverts commit 7257183eb76a1320695385ba4317149e70f946c1.

--- my-app/src/pages/WebToonPage/WebToonPage.jsx
+++ my-app/src/pages/WebToonPage/WebToonPage.jsx
@@ -1,36 +1,32 @@
 import React, { useState, useEffect } from "react";
-import { useRouter } from "next/router";
 import Header from "../Header/header";
 import Footer from "../Footer/footer";
-import ClickLayoutComponent from "./ClickLayoutComponent";
+
 import WebToonPageCss from "./styles/WebToonPageCss.css";
+import MainPageCss from "./styles/MainPageCss.css";
+import ClickLayoutComponent from "./ClickLayoutComponent";
 
 const WebToonPage = () => {
-  const router = useRouter();
-  const { webtoonId } = router.query;
-
   const [webtoons, setWebtoons] = useState([]);
   const [isVisible, setIsVisible] = useState(true);
   const [selectedWebtoon, setSelectedWebtoon] = useState(null);
-  const [ep, setEp] = useState(null); // ep 값을 상태로 관리
 
   useEffect(() => {
     fetch("/api/daywebtoon?day=All")
       .then((response) => response.json())
       .then((data) => {
         setWebtoons(data.webtoons);
-        const selectedWebtoon = data.webtoons.find((webtoon) => webtoon.id === webtoonId);
-        setEp(selectedWebtoon.ep);
       })
       .catch((error) => {
         console.error("Error fetching API:", error);
       });
-  }, [webtoonId]);
+  }, []);
 
   const handleWebToonCutClick = (webtoon) => {
     setSelectedWebtoon(webtoon);
     setIsVisible(true);
 
+    // 토글이 일정 시간(1.5초) 후에 자동으로 사라지도록 설정
     setTimeout(() => {
       setIsVisible(false);
     }, 2500);
@@ -53,8 +49,11 @@ const WebToonPage = () => {
             <img src="1.jpg" alt="1컷" />
           </div>
         ))}
+
       </div>
-      {selectedWebtoon && isVisible && <ClickLayoutComponent ep={webtoonId} />}
+      {selectedWebtoon && isVisible && (
+        <ClickLayoutComponent />
+      )}
       <Footer />
     </div>
   );
