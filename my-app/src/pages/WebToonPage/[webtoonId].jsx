2a8a2839a10cec4300eecb3f14d20da03c50d6ec
리진아 <mnb2098@naver.com>
2023-07-13 12:31:35 +0900

    웹툰 이미지 전체 넣기

--- my-app/src/pages/WebToonPage/[webtoonId].jsx
+++ my-app/src/pages/WebToonPage/[webtoonId].jsx
@@ -42,16 +42,13 @@ const WebToonPage = () => {
   // episodeNumber 추가
   const getWebtoonImage = (webtoon, episodeNumber) => {
     const webtoonImages = {
-      //제목이 똑 닮은 딸이고
       "똑 닮은 딸": {
-        //episodeNumber가 1화일 때 
         1: [
           "/WebtoonImg/web1/web1_1/web1_1_1.png",
           "/WebtoonImg/web1/web1_1/web1_1_2.png",
           "/WebtoonImg/web1/web1_1/web1_1_3.png",
           "/WebtoonImg/web1/web1_1/web1_1_4.png"
         ],
-        //2화일 때 
         2: [
           "/WebtoonImg/web1/web1_2/web1_2_1.png",
           "/WebtoonImg/web1/web1_2/web1_2_2.png",
@@ -65,49 +62,61 @@ const WebToonPage = () => {
           "/WebtoonImg/web1/web1_3/web1_3_3.png",
           "/WebtoonImg/web1/web1_3/web1_3_4.png",
           "/WebtoonImg/web1/web1_3/web1_3_5.png"
-        ],
+        ]
       },
-      //제목이 마루는 강쥐고
       "마루는 강쥐": {
-        //1화일 때 
         1: [
           "/WebtoonImg/web2/web2_1/web2_1_1.png",
           "/WebtoonImg/web2/web2_1/web2_1_2.png",
           "/WebtoonImg/web2/web2_1/web2_1_3.png"
-          ],
+        ],
         2: [
           "/WebtoonImg/web2/web2_2/web2_2_1.png",
           "/WebtoonImg/web2/web2_2/web2_2_2.png",
           "/WebtoonImg/web2/web2_2/web2_2_3.png",
           "/WebtoonImg/web2/web2_2/web2_2_4.png"
-          ],
+        ],
         3: [
           "/WebtoonImg/web2/web2_3/web2_3_1.png",
           "/WebtoonImg/web2/web2_3/web2_3_2.png",
           "/WebtoonImg/web2/web2_3/web2_3_3.png"
-          ],
+        ]
       },
       "소녀재판": {
         1: [
           "/WebtoonImg/web3/web3_1/web3_1_1.png",
           "/WebtoonImg/web3/web3_1/web3_1_2.png",
           "/WebtoonImg/web3/web3_1/web3_1_3.png"
-          ],
+        ],
         2: [
           "/WebtoonImg/web3/web3_2/web3_2_1.png",
           "/WebtoonImg/web3/web3_2/web3_2_2.png",
           "/WebtoonImg/web3/web3_2/web3_2_3.png"
-          ],
+        ],
         3: [
           "/WebtoonImg/web3/web3_3/web3_3_1.png",
           "/WebtoonImg/web3/web3_3/web3_3_2.png",
           "/WebtoonImg/web3/web3_3/web3_3_3.png"
-          ],
+        ]
       },
+    "신혼일기":{
+        1: [
+          "/WebtoonImg/web4/web4_1/web4_1_1.png",
+          "/WebtoonImg/web4/web4_1/web4_1_2.png",
+          "/WebtoonImg/web4/web4_1/web4_1_3.png"
+        ],
+        2: [
+          "/WebtoonImg/web4/web4_2/web4_2_1.png",
+          "/WebtoonImg/web4/web4_2/web4_2_2.png",
+          "/WebtoonImg/web4/web4_2/web4_2_3.png"
+        ]
+      }
     };
-
-    return webtoonImages[webtoon.webtoon_name]?.[episodeNumber] || "";
+  
+    return webtoonImages[webtoon.webtoon_name]?.[episodeNumber] || [];
   };
+  
+  
 
   const handleWebToonCutClick = (webtoon) => {
     setSelectedWebtoon(webtoon);
@@ -122,26 +131,27 @@ const WebToonPage = () => {
     setIsVisible(!isVisible);
   };
 
-
   return (
     <div className="WebToonPage" onClick={handleScreenClick}>
       <Header />
       <div className="WebToonBox">
-        {webtoons.map((webtoon, index) => (
-          <div className="WebToonCut" key={index} onClick={() => handleWebToonCutClick(webtoon)}>
-            {(webtoon.webtoon_name === "똑 닮은 딸" && episodeNumber >= 1 && episodeNumber <= 5) ? (
-              getWebtoonImage(webtoon, episodeNumber).map((image, imageIndex) => (
-                <img key={imageIndex} src={image} alt={`Webtoon Image ${imageIndex}`} />
-              ))
-            ) : (
-              <img src={getWebtoonImage(webtoon, episodeNumber)} alt="Webtoon Image" />
-            )}
-          </div>
-        ))}
+      {webtoons.map((webtoon, index) => (
+        <div className="WebToonCut" key={index} onClick={() => handleWebToonCutClick(webtoon)}>
+          {webtoon.webtoon_name === webtoonName && episodeNumber ? (
+            //getWebtoonImage 함수 교체
+            getWebtoonImage(webtoon, episodeNumber).map((image, imageIndex) => (
+              <img key={imageIndex} src={image} alt={`Webtoon Image ${imageIndex}`} />
+            ))
+          ) : (
+            <img src={getWebtoonImage(webtoon, 1)[0]} alt="Webtoon Image" />
+          )}
+        </div>
+      ))}
       </div>
       {selectedWebtoon && isVisible && <ClickLayoutComponent webtoonName={webtoonName} episodeNumber={episodeNumber} maxEp={count} />}
       <Footer />
     </div>
   );
 };
+
 export default WebToonPage;
