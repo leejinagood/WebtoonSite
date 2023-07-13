f1ca7bd7673b16345b88cfeaa01617b400adbc3e
리진아 <mnb2098@naver.com>
2023-07-13 10:45:32 +0900

    웹툰 보여주기

--- my-app/src/pages/WebToonPage/[webtoonId].jsx
+++ my-app/src/pages/WebToonPage/[webtoonId].jsx
@@ -7,7 +7,8 @@ import WebToonPageCss from "./styles/WebToonPageCss.css";
 
 const WebToonPage = () => {
   const router = useRouter();
-  const { webtoonName, episodeNumber } = router.query;
+  const { webtoonName } = router.query;
+  const episodeNumber = typeof router.query.episodeNumber === "string" ? parseInt(router.query.episodeNumber) : undefined;
 
   const [webtoons, setWebtoons] = useState([]);
   const [isVisible, setIsVisible] = useState(true);
@@ -20,9 +21,7 @@ const WebToonPage = () => {
         const response = await fetch(`/api/webtoondetail?name=${encodeURIComponent(webtoonName)}`);
         const data = await response.json();
         const { webtoons } = data;
-        //data 의 webtoon_name을 찾아 맞는 webtoon_name를
         const selectedWebtoon = webtoons.find((webtoon) => webtoon.webtoon_name === webtoonName);
-        // selectedWebtoon로 할당
         setSelectedWebtoon(selectedWebtoon);
         setWebtoons(webtoons);
         setCount(selectedWebtoon?.count || 0);
@@ -30,22 +29,32 @@ const WebToonPage = () => {
         console.error("Error fetching API:", error);
       }
     };
-  
+
     if (webtoonName) {
       fetchData();
     }
   }, [webtoonName]);
-  
-  // 기존 코드에서 간소화
-  const getWebtoonImage = (webtoon) => {
+
+  const getWebtoonImage = (webtoon, episodeNumber) => {
     const webtoonImages = {
-      "똑 닮은 딸": "/WebtoonImg/web1/web1_1/web1_1_1.png",
-      "마루는 강쥐": "/WebtoonImg/web2/web2_thumbnail.jpg",
-      "소녀재판": "/WebtoonImg/web3/web3_1/web3_1_1.png",
+      "똑 닮은 딸": {
+        1: [
+          "/WebtoonImg/web1/web1_1/web1_1_1.png",
+          "/WebtoonImg/web1/web1_1/web1_1_2.png",
+          "/WebtoonImg/web1/web1_1/web1_1_3.png",
+          "/WebtoonImg/web1/web1_1/web1_1_4.png"
+        ],
+      },
+      "마루는 강쥐": {
+        1: ["/WebtoonImg/web2/web2_thumbnail.jpg"],
+      },
+      "소녀재판": {
+        1: "/WebtoonImg/web3/web3_1/web3_1_1.png",
+      },
     };
-    return webtoonImages[webtoon.webtoon_name] || "1.jpg";
+
+    return webtoonImages[webtoon.webtoon_name]?.[episodeNumber] || "1.jpg";
   };
-  
 
   const handleWebToonCutClick = (webtoon) => {
     setSelectedWebtoon(webtoon);
@@ -65,12 +74,14 @@ const WebToonPage = () => {
       <Header />
       <div className="WebToonBox">
         {webtoons.map((webtoon, index) => (
-          <div
-            className="WebToonCut"
-            key={index}
-            onClick={() => handleWebToonCutClick(webtoon)}
-          >
-            <img src={getWebtoonImage(webtoon)}/>
+          <div className="WebToonCut" key={index} onClick={() => handleWebToonCutClick(webtoon)}>
+            {webtoon.webtoon_name === "똑 닮은 딸" && episodeNumber === 1 ? (
+              getWebtoonImage(webtoon, episodeNumber).map((image, imageIndex) => (
+                <img key={imageIndex} src={image} alt={`Webtoon Image ${imageIndex}`} />
+              ))
+            ) : (
+              <img src={getWebtoonImage(webtoon, episodeNumber)} alt="Webtoon Image" />
+            )}
           </div>
         ))}
       </div>
