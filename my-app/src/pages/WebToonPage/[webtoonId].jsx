78f2e14533ea2ce16fd55fdc1839c8f19e185434
리진아 <mnb2098@naver.com>
2023-07-13 17:21:57 +0900

    코드 주석 추가

--- my-app/src/pages/WebToonPage/[webtoonId].jsx
+++ my-app/src/pages/WebToonPage/[webtoonId].jsx
@@ -42,31 +42,31 @@ const WebToonPage = () => {
   // 이미지 url 코드 최소화
   const getWebtoonImage = (webtoon, episodeNumber) => {
     const webtoonImages = {
-      "똑 닮은 딸": {
-        1: Array.from({ length: 102 }, (_, i) => `/WebtoonImg/web1/web1_1/web1_1_${i + 1}.png`),
-        2: Array.from({ length: 5 }, (_, i) => `/WebtoonImg/web1/web1_2/web1_2_${i + 1}.png`),
-        3: Array.from({ length: 5 }, (_, i) => `/WebtoonImg/web1/web1_3/web1_3_${i + 1}.png`),
+      "똑 닮은 딸": { // 제목이 똑 닮은 딸이고
+        1: Array.from({ length: 102 }, (_, i) => `/WebtoonImg/web1/web1_1/web1_1_${i + 1}.png`), // 1화일 때
+        2: Array.from({ length: 5 }, (_, i) => `/WebtoonImg/web1/web1_2/web1_2_${i + 1}.png`), // 2화일 때
+        3: Array.from({ length: 5 }, (_, i) => `/WebtoonImg/web1/web1_3/web1_3_${i + 1}.png`), // 3화일 때
       },
-      "마루는 강쥐": {
+      "마루는 강쥐": { // 제목이 마루는 강쥐이고 
         1: Array.from({ length: 3 }, (_, i) => `/WebtoonImg/web2/web2_1/web2_1_${i + 1}.png`),
         2: Array.from({ length: 4 }, (_, i) => `/WebtoonImg/web2/web2_2/web2_2_${i + 1}.png`),
         3: Array.from({ length: 3 }, (_, i) => `/WebtoonImg/web2/web2_3/web2_3_${i + 1}.png`),
       },
-      "소녀재판": {
+      "소녀재판": { // 제목이 소녀재판이고
         1: Array.from({ length: 3 }, (_, i) => `/WebtoonImg/web3/web3_1/web3_1_${i + 1}.png`),
         2: Array.from({ length: 3 }, (_, i) => `/WebtoonImg/web3/web3_2/web3_2_${i + 1}.png`),
         3: Array.from({ length: 3 }, (_, i) => `/WebtoonImg/web3/web3_3/web3_3_${i + 1}.png`),
       },
-      "신혼일기": {
+      "신혼일기": { // 제목이 신혼일기이고
         1: Array.from({ length: 3 }, (_, i) => `/WebtoonImg/web4/web4_1/web4_1_${i + 1}.png`),
         2: Array.from({ length: 3 }, (_, i) => `/WebtoonImg/web4/web4_2/web4_2_${i + 1}.png`),
       },
-      "외모지상주의": {
+      "외모지상주의": { // 제목이 외지주이고 
         1: Array.from({ length: 6 }, (_, i) => `/WebtoonImg/web5/web5_1/web5_1_${i + 1}.png`),
         2: Array.from({ length: 5 }, (_, i) => `/WebtoonImg/web5/web5_2/web5_2_${i + 1}.png`),
         3: Array.from({ length: 3 }, (_, i) => `/WebtoonImg/web5/web5_3/web5_3_${i + 1}.png`),
       },
-      "퀘스트지상주의": {
+      "퀘스트지상주의": { // 제목이 퀘스트지상주의이고 
         1: Array.from({ length: 3 }, (_, i) => `/WebtoonImg/web6/web6_1/web6_1_${i + 1}.png`),
         2: Array.from({ length: 4 }, (_, i) => `/WebtoonImg/web6/web6_2/web6_2_${i + 1}.png`),
       },
