523e63615448774795bb381a5cbe180d2a117f6a
solbin <126735005+solbinsol@users.noreply.github.com>
2023-07-14 15:44:26 +0900

    ㅅㄹㅇㄷ

--- my-app/src/item/NewToon.jsx
+++ my-app/src/item/NewToon.jsx
@@ -1,8 +1,10 @@
 import React, { useState, useEffect } from "react";
-import NewToonCss from "../styles/NewToonCss.css";
+import "../styles/NewToonCss.css";
 
 const NewToon = () => {
   const [result, setResult] = useState([]);
+  const [activeIndex, setActiveIndex] = useState(0);
+  const [slideAnimation, setSlideAnimation] = useState(""); // 슬라이드 애니메이션 클래스
 
   useEffect(() => {
     const fetchData = async () => {
@@ -19,13 +21,34 @@ const NewToon = () => {
     fetchData();
   }, []);
 
+  const handleImageClick = (index) => {
+    if (index === activeIndex) return;
+
+    const slides = result.slice();
+    const activeSlide = slides.splice(index, 1)[0];
+    slides.splice(1, 0, activeSlide);
+
+    setResult(slides);
+    setActiveIndex(1);
+
+    setSlideAnimation("slide-animation"); // 슬라이드 애니메이션 클래스 추가
+    setTimeout(() => {
+      setSlideAnimation(""); // 일정 시간 후에 슬라이드 애니메이션 클래스 제거
+    }, 500); // 애니메이션 시간 설정 (0.5초)
+  };
+
   return (
-    <div className="NewToon">
-      <div className="slider">
+    <div className="NewToonPage">
+      <div className={`slider ${slideAnimation}`}>
         {result.map((title, index) => (
-          <div className="NewToonInfo" key={index}>
-            <img src={`${index + 1}.jpg`} alt="" />
-            <h3 className="NewToonTitle">{title}</h3>
+          <div
+            className={`NewToonInfo ${activeIndex === index ? "active" : ""}`}
+            key={index}
+            onClick={() => handleImageClick(index)}
+          >
+            <div className="NewToonItem">
+              <img src={`/${index + 1}.jpg`} alt={title} />
+            </div>
           </div>
         ))}
       </div>
