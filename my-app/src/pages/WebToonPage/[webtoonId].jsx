7670a6bdc1a308f2ebdee53e94cd2b5c71bddbb6
리진아 <mnb2098@naver.com>
2023-07-14 14:58:29 +0900

    주석 추가

--- my-app/src/pages/WebToonPage/[webtoonId].jsx
+++ my-app/src/pages/WebToonPage/[webtoonId].jsx
@@ -23,19 +23,19 @@ const WebToonPage = () => {
         const response = await fetch(`/api/webtoondetail?name=${encodeURIComponent(webtoonName)}`);
         const data = await response.json();
         const { webtoons } = data;
-        //data 의 webtoon_name을 찾아 맞는 webtoon_name를
+         //data 의 webtoon_name을 찾아 맞는 webtoon_name를
         const selectedWebtoon = webtoons.find((webtoon) => webtoon.webtoon_name === webtoonName);
         // selectedWebtoon로 할당
         setSelectedWebtoon(selectedWebtoon);
         setWebtoons(webtoons);
         setCount(selectedWebtoon?.count || 0);
       } catch (error) {
-        console.error("Error fetching API:", error);
+        console.error("API 호출 오류:", error);
       }
     };
 
     const fetchImg = async () => {
-      try { 
+      try {
         // 받아온 웹툰 제목과 에피소드 번호를 파라미터에 할당하여 
         const response = await fetch(`/api/Webtoon_Img?webtoonName=${webtoonName}&episodeNumber=${episodeNumber}`);
         //값을 data에 저장
@@ -45,23 +45,17 @@ const WebToonPage = () => {
         const episodeImage = EpisodeImg[0].Episode_Image;
         //이미지 갯수
         const episodeImageCount = EpisodeImg[0].Episode_Img_Count;
-
         //url을 생성하여 이미지 배열에 저장
-        const images = [];
-        for (let i = 1; i <= episodeImageCount; i++) {
-          // 반복문으로 1부터 이미지 카운트 갯수만큼
-          const imageUrl = `${episodeImage}${i}.png`;
-          images.push(imageUrl);
-        }
-        console.log(episodeImage, episodeImageCount);
+        // 반복문으로 1부터 이미지 카운트 갯수만큼
+        const images = Array.from({ length: episodeImageCount }, (_, i) => `${episodeImage}${i + 1}.png`);
         setWebtoonImages(images);
       } catch (error) {
-        console.error("Error fetching API:", error);
+        console.error("API 호출 오류:", error);
       }
     };
 
+    //선택된 웹툰을 selectedWebtoon 상태로 설정, 모든 웹툰 목록을 webtoons 상태로..
     if (webtoonName) {
-      //선택된 웹툰을 selectedWebtoon 상태로 설정, 모든 웹툰 목록을 webtoons 상태로..
       fetchData();
     }
 
