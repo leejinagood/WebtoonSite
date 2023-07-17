069da2fa51d52d6bcec714ae5672158645537c4d
리진아 <mnb2098@naver.com>
2023-07-17 17:24:30 +0900

    메인 화면 이미지 추출 코드 수정

--- my-app/src/item/AllToonInfo.jsx
+++ my-app/src/item/AllToonInfo.jsx
@@ -15,37 +15,14 @@ const AllToonInfo = () => {
       });
   }, []);
 
-  const getThumbnailImage = (webtoon) => {
-    if (webtoon.webtoon_name === "똑 닮은 딸") {
-      return "/WebtoonImg/web1/web1_thumbnail.jpg";
-    } else if (webtoon.webtoon_name === "마루는 강쥐") {
-      return "/WebtoonImg/web2/web2_thumbnail.jpg";
-    } else if (webtoon.webtoon_name === "소녀재판") {
-      return "/WebtoonImg/web3/web3_thumbnail.jpg";
-    } else if (webtoon.webtoon_name === "신혼일기") {
-        return "/WebtoonImg/web4/web4_thumbnail.jpg";
-    } else if (webtoon.webtoon_name === "외모지상주의") {
-      return "/WebtoonImg/web5/web5_thumbnail.jpg";
-    }else if (webtoon.webtoon_name === "퀘스트지상주의") {
-      return "/WebtoonImg/web6/web6_thumbnail.jpg";
-    }
-    // 기본값으로 설정할 썸네일 이미지 경로
-    return "";
-  };
-
   return (
     <div className="ATBox">
       {webtoons.map((webtoon, index) => (
         <div className="AllToonInfo" key={index}>
-
-
           <Link href={`/ListPage/ListPage?webtoonName=${encodeURIComponent(webtoon.webtoon_name)}`}>
-            
-          <img src={getThumbnailImage(webtoon)} alt="" />
-          <p className="ToonTitle">{webtoon.webtoon_name}</p>
-            
+            <Thumbnail webtoon={webtoon} />
+            <p className="ToonTitle">{webtoon.webtoon_name}</p>
           </Link>
-
         </div>
       ))}
       {webtoons.length % 3 !== 0 && (
@@ -53,6 +30,29 @@ const AllToonInfo = () => {
       )}
     </div>
   );
+};
+
+const Thumbnail = ({ webtoon }) => {
+  const [thumbnailSrc, setThumbnailSrc] = useState("");
+
+  useEffect(() => {
+    const fetchThumbnail = async () => {
+      try {
+        const response = await fetch(`/api/Webtoon_Thumbnail?webtoonName=${webtoon.webtoon_name}`);
+        const data = await response.json();
+        const thumbnail = data.rows[0]?.[0]?.Webtoon_Thumbnail;
+        if (thumbnail) {
+          setThumbnailSrc(thumbnail);
+        }
+      } catch (error) {
+        console.error("Error fetching API:", error);
+      }
+    };
+
+    fetchThumbnail();
+  }, [webtoon.webtoon_name]);
+
+  return <img src={thumbnailSrc} alt="" />;
 };
 
 export default AllToonInfo;
