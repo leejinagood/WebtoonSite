2f05efc7f7325a486b207e8957d21ef41d09ea0c
리진아 <mnb2098@naver.com>
2023-07-18 14:44:42 +0900

    day 파라미터 수정

--- my-app/src/pages/DayPage/FirDayPage.jsx
+++ my-app/src/pages/DayPage/FirDayPage.jsx
@@ -5,7 +5,7 @@ import MondayPage from './MonDayPage';
 class TuesDayPage extends Component {
     render() {
       return (
-        <MondayPage week="금" day="fir"/>
+        <MondayPage week="금" day="fri"/>
       );
     }
   }
