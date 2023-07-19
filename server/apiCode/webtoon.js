d36f7c15c8de244307e47178a2cf133c0ee54131
리진아 <mnb2098@naver.com>
2023-07-19 18:31:52 +0900

    서버코드ㅜ

--- server/apiCode/webtoon.js
+++ server/apiCode/webtoon.js
@@ -1,66 +1,36 @@
 //웹툰의 정보를 볼 수 있는 api
 
 const webtoonAPI = (server, getConn) => {
-
-//day를 파라미터로 받아와 웹툰 제목을 출력하는 메서드 
-server.get('/api/daywebtoon', async (req, res) => {
-  const conn = await getConn();
-  const { day } = req.query;
-  const query = 'CALL usp_get_dayWebtoon(?);';
-  const WebtoonDetailquery = 'CALL usp_get_Webtoon_ID(?);';
-  try {
-    const [row] = await conn.query(query, [day]); 
-    const result = row[0].map((row) => row.webtoonID); //day로 출력된 ID를 result값에
-
-    const webtoonDetails = [];
-    for (const webtoonID of result) {
-      const [rows] = await conn.query(WebtoonDetailquery, [webtoonID]);
-      const [row] = rows[0]; //받아온 ID를 다시 WebtoonDetailquery 넣고 추출된 웹툰 제목과 썸네일의 값
-      webtoonDetails.push({
-        webtoon_name: row.webtoonName, // 제목
-        webtoon_en_name: row.webtoonEnName, // 영어제목
-        thumbnail: row.webtoonThumbnail, // 썸네일
-      });
-    }
-    //응답은 웹툰 제목과, 영어제목, 썸네일을 보냄
-    res.send(webtoonDetails);
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: '서버 스크립트의 오류' });
-  } finally {
-    conn.release();
-  }
-});
-
-
-//새롭게 업로드된지 일주일 된 신규 웹툰의 제목을 출력
-server.get('/api/new', async (req, res) => {
-  const conn = await getConn();
-  const query = 'call usp_get_New();';
-  const WebtoonDetailquery = 'CALL usp_get_Webtoon_ID(?);';
-  try{
-  let [rows] = await conn.query(query);
-  const ID = rows[0].map((row) => row.webtoonID); //웹툰 제목만 출력
+  server.get('/api/webtoons', async (req, res) => {
+    const conn = await getConn();
+    const { day } = req.query;
+    const query = day ? 'CALL usp_get_dayWebtoon(?);' : 'call usp_get_New();';
+    const WebtoonDetailquery = 'CALL usp_get_Webtoon_ID(?);';
+    
+    try {
+      let [rows] = await conn.query(query, [day]);
+      const ID = rows[0].map((row) => row.webtoonID);
   
-  const webtoonDetails = [];
-  for (const webtoonID of ID) {
-    const [rows] = await conn.query(WebtoonDetailquery, [webtoonID]);
-    const [row] = rows[0]; //받아온 ID를 다시 WebtoonDetailquery 넣고 추출된 웹툰 제목과 썸네일의 값
-    webtoonDetails.push({
-      webtoon_name: row.webtoonName, // 제목
-      webtoon_en_name: row.webtoonEnName, // 영어제목
-      thumbnail: row.webtoonThumbnail, // 썸네일
-    });
-  }
-  //응답은 웹툰 제목과, 영어제목, 썸네일을 보냄
-  res.send(webtoonDetails);
-  }catch(error) {
-    console.error(error);
-    res.status(500).json({ error: '서버 스크립트의 오류' });
-  } finally {
-    conn.release(); // 연결 해제
-  }
-});
+      const webtoonDetails = [];
+      for (const webtoonID of ID) {
+        const [rows] = await conn.query(WebtoonDetailquery, [webtoonID]);
+        const [row] = rows[0];
+        webtoonDetails.push({
+          webtoon_name: row.webtoonName,
+          webtoon_en_name: row.webtoonEnName,
+          thumbnail: row.webtoonThumbnail,
+        });
+      }
+      
+      res.send(webtoonDetails);
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: '서버 스크립트의 오류' });
+    } finally {
+      conn.release();
+    }
+  });
+  
 
 
 //메인페이지에서 좋아요가 가장 높은 웹툰 중 top5 제목과 작가, 썸네일 출력
