c6c25b312581cb9f3ca651b00f3663cd30672e8a
리진아 <mnb2098@naver.com>
2023-07-20 13:57:07 +0900

    잔수정

--- server/apiCode/webtoonList.js
+++ server/apiCode/webtoonList.js
@@ -16,23 +16,22 @@ const webtoonListAPI = (server, getConn) => {
         const ID = rows[0].map((row) => row.webtoonID); // ID를 추출
 
         const webtoonDetails = []; // 배열로 초기화
-      for (const webtoonID of ID) { // 요일별 웹툰과 신규 웹툰 전부 ID를 받음.
-        const [rows] = await conn.query(webtoonQuery, [webtoonID]);
-        const [row] = rows[0]; // 배열의 첫번째 부분 
-        webtoonDetails.push({
-          webtoon_name: row.webtoonName, // 웹툰 제목과
-          webtoon_en_name: row.webtoonEnName, // 웹툰 영어 제목과
-          thumbnail: row.webtoonThumbnail, // 웹툰 썸네일을 추출
-          author: row.webtoonAuthor, // 웹툰 작가 추출
-          week: row.webtoonWeek, // 무슨 요일에 연재하는지
-          content: row.webtoonContent, //웹툰 상세 내용
-          like: row.LikesCount // 좋아요 갯수
-        });
-      }
-      console.log(ID);
-      console.log(webtoonDetails);
-      res.send(webtoonDetails); // 응답으로 보냄
-
+        for (const webtoonID of ID) { // 요일별 웹툰과 신규 웹툰 전부 ID를 받음.
+            const [rows] = await conn.query(webtoonQuery, [webtoonID]);
+            const [row] = rows[0]; // 배열의 첫번째 부분 
+            webtoonDetails.push({
+            webtoon_name: row.webtoonName, // 웹툰 제목과
+            webtoon_en_name: row.webtoonEnName, // 웹툰 영어 제목과
+            thumbnail: row.webtoonThumbnail, // 웹툰 썸네일을 추출
+            author: row.webtoonAuthor, // 웹툰 작가 추출
+            week: row.webtoonWeek, // 무슨 요일에 연재하는지
+            content: row.webtoonContent, //웹툰 상세 내용
+            like: row.LikesCount // 좋아요 갯수
+            });
+        }
+        console.log(ID);
+        console.log(webtoonDetails);
+        res.send(webtoonDetails); // 응답으로 보냄
     } catch (error) {
         console.error(error);
         res.status(500).send({ error: '서버 스크립트의 오류' });
@@ -41,6 +40,42 @@ const webtoonListAPI = (server, getConn) => {
     }
     });
 
+
+
+    server.get('/api/webtoonlist', async (req, res) => {
+        const conn = await getConn();
+        const { EnName } = req.query; //영어이름
+        const query = 'call usp_get_webtoonID_EnName (?);';
+        const webtoonQuery = 'CALL usp_get_webtoonDetail_ID(?);'; // ID를 받아와 웹툰 정보를 출력하는 SP
+    
+        try {
+            let [rows] = await conn.query(query, [EnName]); // EnName 파라미터로 받아온 후
+            const ID = rows[0].map((row) => row.webtoonID); // ID를 추출
+    
+            const webtoonDetails = []; // 배열로 초기화
+            for (const webtoonID of ID) { // 요일별 웹툰과 신규 웹툰 전부 ID를 받음.
+                const [rows] = await conn.query(webtoonQuery, [webtoonID]);
+                const [row] = rows[0]; // 배열의 첫번째 부분 
+                webtoonDetails.push({
+                webtoon_name: row.webtoonName, // 웹툰 제목과
+                webtoon_en_name: row.webtoonEnName, // 웹툰 영어 제목과
+                thumbnail: row.webtoonThumbnail, // 웹툰 썸네일을 추출
+                author: row.webtoonAuthor, // 웹툰 작가 추출
+                week: row.webtoonWeek, // 무슨 요일에 연재하는지
+                content: row.webtoonContent, //웹툰 상세 내용
+                like: row.LikesCount // 좋아요 갯수
+                });
+            }
+            console.log(ID);
+            console.log(webtoonDetails);
+            res.send(webtoonDetails); // 응답으로 보냄
+        } catch (error) {
+            console.error(error);
+            res.status(500).send({ error: '서버 스크립트의 오류' });
+        } finally {
+            conn.release(); // 연결 해제
+        }
+        });
 
 
 
