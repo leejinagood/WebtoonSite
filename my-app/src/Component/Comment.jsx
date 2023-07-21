db0ee50a2f1cc2fb503a9351e0eb4090084902d0
solbin <126735005+solbinsol@users.noreply.github.com>
2023-07-21 14:54:58 +0900

    커멘트 고치는중

--- my-app/src/Component/Comment.jsx
+++ my-app/src/Component/Comment.jsx
@@ -22,13 +22,16 @@ const Comment = ({ webtoonName, episodeNumber }) => {
       }),
     })
       .then((response) => response.json())
-.then((data) => {
-  console.log(data); // 성공적으로 업로드되었을 때의 처리
-  // 업로드가 성공했다는 메시지를 사용자에게 표시할 수 있습니다.
+      .then((data) => {
+        console.log(data); // 성공적으로 업로드되었을 때의 처리
+        // 업로드가 성공했다는 메시지를 사용자에게 표시할 수 있습니다.
 
-  // 새로운 코멘트 컨텐츠를 리스트에 추가
-  setComments((prevComments) => [...prevComments, { Comment_Content: commentContent }]);
-})
+        // 새로운 코멘트 컨텐츠를 리스트에 추가
+        setComments((prevComments) => [...prevComments, data]); // data를 comments에 추가
+      })
+      .catch((error) => {
+        console.error("Error uploading comment:", error);
+      });
   };
 
   useEffect(() => {
@@ -38,33 +41,43 @@ const Comment = ({ webtoonName, episodeNumber }) => {
         const { comment } = data;
         console.log(webtoonName, episodeNumber);
         console.log(comment);
+
+        // API 응답으로 받아온 코멘트 목록을 comments 상태에 업데이트
+        setComments(comment);
       })
       .catch((error) => {
         console.error("Error fetching API:", error);
       });
-
   }, [webtoonName, episodeNumber]);
 
   console.log(webtoonName);
   console.log(episodeNumber);
-  return (
+return (
     <div className="CommentComponent">
       <div className="Comment">
         <div className="CommentList">
           <ul>
-          {comments.map((comment, index) => (
-  <li key={index}>
-    <span className="NameDay">
-      {comment && comment.User_Name && comment.Comment_Date ? `${comment.User_Name}/${comment.Comment_Date}` : ''}
-    </span>
-    <br />
-    <span className="Comment_Content">{comment && comment.Comment_Content}</span>
-  </li>
-))}
+            {comments.length > 0 ? (
+              comments.map((comment, index) => (
+                <li key={index}>
+                  <span className="NameDay">
+                    {comment && comment.User_Name && comment.Comment_Date
+                      ? `${comment.User_Name}/${comment.Comment_Date}`
+                      : ''}
+                  </span>
+                  <br />
+                  <span className="Comment_Content">
+                    {comment && comment.Comment_Content}
+                  </span>
+                </li>
+              ))
+            ) : (
+              <li>No comments yet.</li>
+            )}
           </ul>
         </div>
         <div className="CommentBox">
-        <textarea ref={commentInputRef} defaultValue=""></textarea>
+          <textarea ref={commentInputRef} defaultValue=""></textarea>
           <button className="CommentUpload" type="submit" onClick={submitComment}>
             작성
           </button>
--- my-app/src/pages/webtoonpage/index.jsx
+++ my-app/src/pages/webtoonpage/index.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect ,useRef } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { useRouter } from "next/router";
 import Header from "@/src/Header/header";
 import Footer from "@/src/Footer/footer";
@@ -7,36 +7,25 @@ import Comment from "@/src/Component/Comment";
 import ClickLayoutComponent from "@/src/Component/ClickLayoutComponent";
 import WebToonPageCss from "./styles/WebToonPageCss.css";
 
-
-const webtoonpage
- = () => {
-
-  
-
+const WebtoonPage = () => {
   const router = useRouter();
-  const { EnName ,ep} = router.query;
-  //undefined일 때 경우 추가
-  
-  
-  // const episodeNumber = typeof router.query.episodeNumber === "string" ? parseInt(router.query.episodeNumber) : undefined;
+  const { EnName, ep } = router.query;
+
   const [webtoons, setWebtoons] = useState([]);
-  // parseInt 함수를 사용하여 문자열로 변환된 경우에만 숫자로 변환하도록
   const [isVisible, setIsVisible] = useState(true);
   const [selectedWebtoon, setSelectedWebtoon] = useState(null);
   const [count, setCount] = useState(0);
   const [webtoonImages, setWebtoonImages] = useState([]);
 
-
   useEffect(() => {
-    
     const fetchData = async () => {
       try {
         const response = await fetch(`/api/webtoon?webtoonName=${EnName}&${ep}`);
         const data = await response.json();
         const { webtoons } = data;
-         //data 의 webtoon_name을 찾아 맞는 webtoon_name를
-        const selectedWebtoon = webtoons.find((webtoon) => webtoon.webtoon_name === webtoonName);
-        // selectedWebtoon로 할당
+        const selectedWebtoon = webtoons.find(
+          (webtoon) => webtoon.webtoon_name === EnName
+        );
         setSelectedWebtoon(selectedWebtoon);
         setWebtoons(webtoons);
         setCount(selectedWebtoon?.count || 0);
@@ -46,40 +35,24 @@ const webtoonpage
     };
 
     const fetchImg = async () => {
-      try {
-        // 받아온 웹툰 제목과 에피소드 번호를 파라미터에 할당하여 
-        const response = await fetch(`/api/Webtoon_Img?webtoonName=${webtoonName}&episodeNumber=${episodeNumber}`);
-        //값을 data에 저장
-        const data = await response.json();
-        const { EpisodeImg } = data;
-        //이미지 경로
-        const episodeImage = EpisodeImg[0].Episode_Image;
-        //이미지 갯수
-        const episodeImageCount = EpisodeImg[0].Episode_Img_Count;
-        //url을 생성하여 이미지 배열에 저장
-        // 반복문으로 1부터 이미지 카운트 갯수만큼
-        // 배열의 길이와 콜백함수를 인자로 받음
-        const images = Array.from({ length: episodeImageCount }, (_, i) => `${episodeImage}${i + 1}.png`); //배열 + 1
-        setWebtoonImages(images);
-      } catch (error) {
-        console.error("API 호출 오류:", error);
+      const images = [];
+      // webtoonImages 설정
+      for (let i = 1; i <= count; i++) {
+        const response = await fetch(
+          `/WebtoonImg/${EnName}/${ep}/${EnName}_${i}_.png`
+        );
+        const blob = await response.blob();
+        const imageUrl = URL.createObjectURL(blob);
+        images.push(imageUrl);
       }
+      setWebtoonImages(images);
     };
 
-
-
-
-    
-    //선택된 웹툰을 selectedWebtoon 상태로 설정, 모든 웹툰 목록을 webtoons 상태로..
-    if (EnName) {
-      fetchData();
-    }
-
     if (EnName && ep) {
-      // 가져온 api를 가지고 webtoonImages 설정
+      fetchData();
       fetchImg();
     }
-  }, [EnName, ep]);
+  }, [EnName, ep, count]);
 
   const handleWebToonCutClick = (webtoon) => {
     setSelectedWebtoon(webtoon);
@@ -97,31 +70,29 @@ const webtoonpage
   return (
     <div className="WebToonPage">
       <Header />
-      <div className="WebToonBox" >
+      <div className="WebToonBox">
         {/* 각 이미지를 배열로 순회 */}
-        {webtoons.map((webtoon, index) => (
-          <div className="WebToonCut" key={index} onClick={() => handleWebToonCutClick(webtoon)}>
-            {/* 주소창에 받아온 내용과 일치하는지 */}
-            {webtoon.webtoon_name === webtoonName && episodeNumber ? (
-              // key 속성을 사용하여 각 이미지를 고유하게 식별
-              webtoonImages.map((image, imageIndex) => (
-                // imageIndex는 순서를 나타내기 위함
-                <img key={imageIndex} src={image} alt={`Webtoon Image ${imageIndex}`} />
-              ))
-            ) : (
-              <img src="" alt="Webtoon Image" />
-            )}
+        {webtoonImages.map((imageUrl, index) => (
+          <div
+            className="WebToonCut"
+            key={index}
+            onClick={() => handleWebToonCutClick(selectedWebtoon)}
+          >
+            <img src={imageUrl} alt={`Webtoon Image ${index}`} />
           </div>
         ))}
       </div>
       {selectedWebtoon && isVisible && (
-        <ClickLayoutComponent webtoonName={webtoonName} episodeNumber={episodeNumber} maxEp={count} />
+        <ClickLayoutComponent
+          webtoonName={selectedWebtoon.webtoon_name}
+          episodeNumber={selectedWebtoon.ep}
+          maxEp={count}
+        />
       )}
       <Comment webtoonName={EnName} episodeNumber={ep} />
       <Footer />
     </div>
   );
-  
 };
 
-export default webtoonpage;
+export default WebtoonPage;
