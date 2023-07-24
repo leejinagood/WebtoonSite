9c7914472576b34eeb1d7591f01a6f334f5dc0fc
리진아 <mnb2098@naver.com>
2023-07-24 14:22:06 +0900

    카카오 로그인 토큰 발급까지 성공

--- my-app/src/OAuth.js
+++ my-app/src/OAuth.js
@@ -1,4 +1,4 @@
-const CLIENT_ID = "6298e4ccbcce464caa91f6a4a0e9c7a3";
+const CLIENT_ID = "c3f27e51572a41e42f4204019b897192";
 const REDIRECT_URL = "http://localhost:3000";
 
 export const Kakao_Auth_Url = `https://kauth.kakao.com/oauth/authorize?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URL}&response_type=code`;
