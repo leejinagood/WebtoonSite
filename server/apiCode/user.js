742e203d48411553c0cd4796e9d4e7868ac221d1
리진아 <mnb2098@naver.com>
2023-07-25 11:00:29 +0900

    카카오 토큰 유효성 검증 코드

--- server/apiCode/user.js
+++ server/apiCode/user.js
@@ -74,7 +74,7 @@ const userAPI = (server, getConn) => {
         token = jwt.sign(
           { UserId: selectUserResult[0].userID, UserEmail: selectUserResult[0].userEmail },
           'your-secret-key', // 비밀키
-          { expiresIn: '30m' } // 토큰 만료 시간 30분 설정
+          { expiresIn: '1m' } // 토큰 만료 시간 30분 설정
         );
         
         // 쿠키로 헤더에 데이터를 담아 응답 보내기
@@ -231,10 +231,19 @@ const userAPI = (server, getConn) => {
       // 쿠키가 존재하는 경우 처리
       const token = DelisousCookie(cookies); // 쿠키에서 토큰 추출
       const Ktoken = KakaoCookie(cookies); // 쿠키에서 카카오 토큰 추출
-      if (token) {
+      if (Ktoken) {//카카오 토큰이 있을 경우 
+        const response = await axios.get('https://kapi.kakao.com/v1/user/access_token_info', {
+          headers: {
+            Authorization: `Bearer ${Ktoken}`,
+          },
+        });
+        // console.log(response);
+        //토큰 인증이 성공하면 응답
+        res.send('토큰 인증 성공');
+      } else if(token){ 
         try {
           // verify가 만료됐는지 확인
-          const TokenA = jwt.verify(token, 'your-secret-key');
+          jwt.verify(token, 'your-secret-key');
           // 토큰이 유효한 경우
           // const userID = TokenA.userID;
           res.send('토큰 인증 성공');
@@ -242,10 +251,6 @@ const userAPI = (server, getConn) => {
           // 토큰이 유효하지 않은 경우
           res.status(401).send('토큰 인증 실패');
         }
-      } else if(Ktoken){ //카카오 토큰이 있을 경우 (코드 수정 필요)
-
-        
-          res.send('토큰 인증 성공');
       } else {
         // 토큰이 없는 경우 처리
         res.status(401).send('쿠키에 토큰이 없음');
