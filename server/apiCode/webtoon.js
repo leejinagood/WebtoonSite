0a76931aca374e5eec2d6897b6307965f3a4934a
리진아 <mnb2098@naver.com>
2023-07-27 14:54:15 +0900

    redis 초기설정

--- server/apiCode/webtoon.js
+++ server/apiCode/webtoon.js
@@ -2,35 +2,56 @@
 
 const webtoonAPI = (server, getConn) => {
 
-  //요일별 웹툰, 전체 웹툰 중 신규웹툰, 좋아요 상위 5개 웹툰
+  const redis = require('redis');
+
+  //Redis 클라이언트 초기화
+  const Client = redis.createClient({
+    host:"127.0.0.1",
+    port:"6379",
+    db:"0"
+  });
+
+  // Redis 클라이언트 연결
+  Client.connect((err) => {
+    if (err) {
+      console.error('Redis 연결 오류:', err);
+    } else {
+      console.log('Redis에 연결되었습니다.');
+    }
+  });
+  
+
+
+  // 요일별 웹툰, 전체 웹툰 중 신규웹툰, 좋아요 상위 5개 웹툰
   server.get('/api/webtoons', async (req, res) => {
     const conn = await getConn();
     const { pi_vch_condition } = req.query;
+    const cacheKey = 'data_cache_key'; // Redis에서 사용할 고유한 키
 
-    //1개의 sp를 4개로 나눔
-    //const query = 'CALL usp_get_Webtoon(?);';
+    // 1개의 sp를 4개로 나눔
+    // const query = 'CALL usp_get_Webtoon(?);';
 
-    //파라미터 조건에 맞는 ID를 받아온 후 ID에 맞는 웹툰 정보를 추출하는 sp에 대입
+    // 파라미터 조건에 맞는 ID를 받아온 후 ID에 맞는 웹툰 정보를 추출하는 sp에 대입
     const webtoonQuery = 'CALL usp_get_webtoonDetail_ID(?);'; // ID를 받아와 웹툰 정보를 출력하는 SP
-    
-    try { // pi_vch_condition를 파라미터로 받아온 후 rows에 대입
+
+    try {
       let rows;
       if (pi_vch_condition === 'All') {
-        [rows] = await conn.query('CALL usp_get_AllWebtoons();'); //All일 때 웹툰 전체
+        [rows] = await conn.query('CALL usp_get_AllWebtoons();'); // All일 때 웹툰 전체
       } else if (pi_vch_condition === 'rank') {
-        [rows] = await conn.query('CALL usp_get_Top5LikedWebtoons();'); //rank일 때 웹툰 좋아요 상위 top5
+        [rows] = await conn.query('CALL usp_get_Top5LikedWebtoons();'); // rank일 때 웹툰 좋아요 상위 top5
       } else if (pi_vch_condition === 'new') {
-        [rows] = await conn.query('CALL usp_get_NewWebtoons();'); //new일 때 생성된지 1주일 된 웹툰들
+        [rows] = await conn.query('CALL usp_get_NewWebtoons();'); // new일 때 생성된지 1주일 된 웹툰들
       } else {
-        [rows] = await conn.query('CALL usp_get_WebtoonsByDay(?);', [pi_vch_condition]); //요일받는 파라미터
+        [rows] = await conn.query('CALL usp_get_WebtoonsByDay(?);', [pi_vch_condition]); // 요일받는 파라미터
       }
 
       const ID = rows[0].map((row) => row.webtoonID); // ID를 추출
-  
+
       const webtoonDetails = []; // 배열로 초기화
       for (const webtoonID of ID) { // 요일별 웹툰과 신규 웹툰 전부 ID를 받음.
         const [rows] = await conn.query(webtoonQuery, [webtoonID]);
-        const [row] = rows[0]; // 배열의 첫번째 부분 
+        const [row] = rows[0]; // 배열의 첫번째 부분
         webtoonDetails.push({
           webtoon_name: row.webtoonName, // 웹툰 제목과
           webtoon_en_name: row.webtoonEnName, // 웹툰 영어 제목과
@@ -40,7 +61,18 @@ const webtoonAPI = (server, getConn) => {
           like: row.LikesCount // 좋아요 갯수
         });
       }
-      res.send(webtoonDetails); // 응답으로 보냄
+      
+      // // Redis 캐시 사용
+      // Client.get(cacheKey, (err, reply) => {
+      //   if (reply) {
+      //     const data = JSON.parse(reply);
+      //     res.send(data);
+      //   } else {
+      //     Client.set(cacheKey, JSON.stringify(webtoonDetails), 'EX', 3600);
+      //     res.send(webtoonDetails);
+      //   }
+      // });
+
     } catch (error) {
       console.error(error);
       res.status(500).json({ error: '서버 스크립트의 오류' });
@@ -48,6 +80,7 @@ const webtoonAPI = (server, getConn) => {
       conn.release();
     }
   });
+
 
 
   //검색하면 그 단어를 포함한 웹툰 제목과 영어제목, 썸네일을 출력하는 메서드
