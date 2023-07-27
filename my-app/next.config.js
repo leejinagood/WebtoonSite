a0f85ed631ef17717639eaa74545e297ab1de73e
solbin <126735005+solbinsol@users.noreply.github.com>
2023-07-27 18:39:59 +0900

    pwa

--- my-app/next.config.js
+++ my-app/next.config.js
@@ -8,10 +8,10 @@ module.exports = {
     ];
   },
 
-  // // 기본적인  PWA 설정   //나중에 true 로 바꿔야함
-  // pwa: { 
-  //   dest: 'public',
-  //   register: false,
-  //   skipWaiting: true,
-  // },
+  // 기본적인  PWA 설정   //나중에 true 로 바꿔야함
+  pwa: { 
+    dest: 'public',
+    register: false,
+    skipWaiting: true,
+  },
 };
