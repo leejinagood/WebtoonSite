6a930d0c0f50e6bb66c01cebb03b6dcc48926131
리진아 <mnb2098@naver.com>
2023-07-27 18:17:52 +0900

    redis에 캐싱처리

--- server/apiCode/redis.js
+++ server/apiCode/redis.js
@@ -18,20 +18,4 @@ redisClient.connect((err) => {
   }
 });
 
-// redis에 데이터 저장
-const set = (key, value) => {
-  redisClient.set(key, JSON.stringify(value));
-};
-
-// 저장 된 데이터를 redis에서 가져오는 미들웨어
-const get = (req, res, cacheKey, callback) => {
-  redisClient.get(cacheKey, (error, data) => {
-    if (error) {
-      callback(error, null);
-    } else {
-      callback(null, JSON.parse(data));
-    }
-  });
-};
-
-module.exports = { set, get };
+module.exports = redisClient;
--- server/apiCode/webtoon.js
+++ server/apiCode/webtoon.js
@@ -1,16 +1,13 @@
 //웹툰의 정보를 볼 수 있는 api
 
 const webtoonAPI = (server, getConn) => {
-  
-  const { set, get } = require("./redis");
+
+  const redisClient = require('./redis'); // redis.js 모듈을 가져옴
 
   // 요일별 웹툰, 전체 웹툰 중 신규웹툰, 좋아요 상위 5개 웹툰
   server.get('/api/webtoons', async (req, res) => {
     const conn = await getConn();
     const { pi_vch_condition } = req.query;
-
-    // 1개의 sp를 4개로 나눔
-    // const query = 'CALL usp_get_Webtoon(?);';
 
     // 파라미터 조건에 맞는 ID를 받아온 후 ID에 맞는 웹툰 정보를 추출하는 sp에 대입
     const webtoonQuery = 'CALL usp_get_webtoonDetail_ID(?);'; // ID를 받아와 웹툰 정보를 출력하는 SP
@@ -29,21 +26,30 @@ const webtoonAPI = (server, getConn) => {
 
       const ID = rows[0].map((row) => row.webtoonID); // ID를 추출
 
-      const webtoonDetails = []; // 배열로 초기화
-      for (const webtoonID of ID) { // 요일별 웹툰과 신규 웹툰 전부 ID를 받음.
-        const [rows] = await conn.query(webtoonQuery, [webtoonID]);
-        const [row] = rows[0]; // 배열의 첫번째 부분
-        webtoonDetails.push({
-          webtoon_name: row.webtoonName, // 웹툰 제목과
-          webtoon_en_name: row.webtoonEnName, // 웹툰 영어 제목과
-          thumbnail: row.webtoonThumbnail, // 웹툰 썸네일을 추출
-          author: row.webtoonAuthor, // 웹툰 작가 추출
-          week: row.webtoonWeek, // 무슨 요일에 연재하는지
-          like: row.LikesCount // 좋아요 갯수
-        });
+      const key = `webtoonKey:${pi_vch_condition}`; //redis의 고유 키값
+      let value = await redisClient.get(key); // redis에서 해당 key로 데이터 조회
+
+      if (value) {
+        // 만약 redis에 데이터가 있다면 그대로 반환 
+        res.send(JSON.parse(value)); //문자열을 객체로 변환하여
+      } else {
+        // 만약 redis에 데이터가 없다면 db(mysql)에서 조회
+        const webtoonDetails = [];
+        for (const webtoonID of ID) { // 요일별 웹툰과 신규 웹툰 전부 ID를 받음.
+          const [rows] = await conn.query(webtoonQuery, [webtoonID]);
+          const [row] = rows[0]; // 배열의 첫번째 부분
+          webtoonDetails.push({
+            webtoon_name: row.webtoonName, // 웹툰 제목과
+            webtoon_en_name: row.webtoonEnName, // 웹툰 영어 제목과
+            thumbnail: row.webtoonThumbnail, // 웹툰 썸네일을 추출
+            author: row.webtoonAuthor, // 웹툰 작가 추출
+            week: row.webtoonWeek, // 무슨 요일에 연재하는지
+            like: row.LikesCount // 좋아요 갯수
+          });
+        }
+        await redisClient.set(key, JSON.stringify(webtoonDetails)); // 조회한 데이터를 JSON 형태로 변환하여 redis에 저장
+        res.send(webtoonDetails);
       }
-      
-      res.send(webtoonDetails);
 
     } catch (error) {
       console.error(error);
@@ -52,7 +58,6 @@ const webtoonAPI = (server, getConn) => {
       conn.release();
     }
   });
-
 
 
   //검색하면 그 단어를 포함한 웹툰 제목과 영어제목, 썸네일을 출력하는 메서드
