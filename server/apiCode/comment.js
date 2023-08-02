dbf35a8ea40c9eda2d48ca2de5822e7a0b374334
리진아 <mnb2098@naver.com>
2023-08-02 14:49:54 +0900

    댓글 api 성능 개선을 위해 수정

--- server/apiCode/comment.js
+++ server/apiCode/comment.js
@@ -7,20 +7,11 @@ const commentAPI = (server, getConn) => {
         const conn = await getConn();
         const { EnName, ep } = req.query; //영어이름과 에피소드 몇 화인지 받아옴
         const values = [ EnName, ep ]
-        const epIDQuery = 'call usp_get_EpiosdeID (?, ?);'; //제목과 epNumber로 episodeID 추출
-        const viewCommentQuery = 'call usp_get_comment(?);'; //댓글 조회
+        const viewCommentQuery = 'call usp_get_comment(?, ?);'; //댓글 조회
         try {
-            const [rows] = await conn.query(epIDQuery, values); // Name, Ep 파라미터로 받아온 후
-            const ID = rows[0].map((row) => row.episodeID); // episodeID를 추출
-
-            const[result] = await conn.query(viewCommentQuery, [ID]); //episodeID로 댓글 조회
-            const comment = result[0].map(row => ({
-            Comment_ID : row.commentID, //댓글 고유의 ID
-            Comment_Content: row.commentContent, //댓글 내용
-            Comment_Date: row.commentDate, //댓글을 입력한 날짜
-            User_Name: row.userName  //사용자 닉네임
-            }));
-            res.send(comment); //댓글 내용을 응답으로
+            const [rows] = await conn.query(viewCommentQuery, values); 
+            const row = rows[0];
+            res.send(row); //댓글 내용을 응답으로
         } catch (error) {
             //console.error(error);
             res.status(500).send({ error: '서버 스크립트의 오류' });
@@ -35,21 +26,11 @@ const commentAPI = (server, getConn) => {
     //댓글 입력 메서드 
     server.post('/api/comment_insert', async (req, res) => {
         const conn = await getConn();
-        const { WebEnName, Ep, UserEmail, content } = req.body; // 영어 이름과 유저 이메일을 받아옴
-        const values = [WebEnName, Ep];
-        const user_email = [UserEmail];
-        const Content = [content];
-        const epIDQuery = 'CALL usp_get_EpiosdeID(?, ?);'; //episodeID
-        const userIDQuery = 'CALL usp_get_userID(?);'; //UserID
-        const insertQuery = 'CALL usp_post_comment(?, ?, ?)'; //댓글 입력
+        const {Ep, WebEnName, UserEmail, content } = req.body; // 영어 이름과 유저 이메일을 받아옴
+        const values = [Ep, WebEnName, UserEmail, content ];
+        const insertQuery = 'CALL usp_post_comment(?, ?, ?, ?)'; //댓글 입력
     
         try {
-            const [epID] = await conn.query(epIDQuery, values); // Name, Ep 파라미터로 받아온 후
-            const EpId = epID[0].map((row) => row.episodeID); // episodeID를 추출
-    
-            const [usID] = await conn.query(userIDQuery, user_email); // UserEmail 파라미터로 받아온 후
-            const UsId = usID[0].map((row) => row.userID); // userID를 추출
-
             const Response = await axios.get('http://localhost:4000/api/Token', { //토큰 인증 호출
                 headers: { //헤더에
                     // Cookie: `token=${token}; KakaoToken=${ktoken}; `
@@ -58,7 +39,8 @@ const commentAPI = (server, getConn) => {
             });
 
             if (Response.data === '토큰 인증 성공' || Response.data === '카카오 토큰 인증 성공') { //인증 성공일 때 댓글 달 수 있음
-                await conn.query(insertQuery, [EpId, UsId, Content]); //episodeID, userID, content 입력 후 댓글 삽입
+                await conn.query(insertQuery, values); //episodeID, userID, content 입력 후 댓글 삽입
+
                 res.send('댓글이 성공적으로 작성되었습니다.');  //응답
             } else { //토큰 인증 실패했을 때 
                 res.status(401).send('로그인 하세요');
@@ -72,44 +54,44 @@ const commentAPI = (server, getConn) => {
     });
 
 
-    //댓글 삭제 
-    server.del('/api/comment_delete', async(req, res) => {
-        const conn = await getConn();
-        const { WebEnName, Ep, UserEmail, CommentId } = req.query; // 영어 이름과 유저 이메일을 받아옴
-        const values = [WebEnName, Ep];
-        const user_email = [UserEmail];
-        const comID = [CommentId];
-        const delQuery = 'call usp_del_comment(?, ?, ?);'; //댓글 삭제
-        const epIDQuery = 'CALL usp_get_EpiosdeID(?, ?);'; //episodeID
-        const userIDQuery = 'CALL usp_get_userID(?);'; //UserID
+    // //댓글 삭제 
+    // server.del('/api/comment_delete', async(req, res) => {
+    //     const conn = await getConn();
+    //     const { WebEnName, Ep, UserEmail, CommentId } = req.query; // 영어 이름과 유저 이메일을 받아옴
+    //     const values = [WebEnName, Ep];
+    //     const user_email = [UserEmail];
+    //     const comID = [CommentId];
+    //     const delQuery = 'call usp_del_comment(?, ?, ?);'; //댓글 삭제
+    //     const epIDQuery = 'CALL usp_get_EpiosdeID(?, ?);'; //episodeID
+    //     const userIDQuery = 'CALL usp_get_userID(?);'; //UserID
 
-        try {
-            const [epID] = await conn.query(epIDQuery, values); // Name, Ep 파라미터로 받아온 후
-            const EpId = epID[0].map((row) => row.episodeID); // episodeID를 추출
+    //     try {
+    //         const [epID] = await conn.query(epIDQuery, values); // Name, Ep 파라미터로 받아온 후
+    //         const EpId = epID[0].map((row) => row.episodeID); // episodeID를 추출
     
-            const [usID] = await conn.query(userIDQuery, user_email); // UserEmail 파라미터로 받아온 후
-            const UsId = usID[0].map((row) => row.userID); // userID를 추출
+    //         const [usID] = await conn.query(userIDQuery, user_email); // UserEmail 파라미터로 받아온 후
+    //         const UsId = usID[0].map((row) => row.userID); // userID를 추출
 
-            const Response = await axios.get('http://localhost:4000/api/Token', { //토큰 인증 호출
-                headers: { //헤더에
-                    // Cookie: `token=${token}; KakaoToken=${ktoken}; `
-                    Cookie: req.headers.cookie, // 현재 쿠키를 그대로 전달
-                },
-            });
+    //         const Response = await axios.get('http://localhost:4000/api/Token', { //토큰 인증 호출
+    //             headers: { //헤더에
+    //                 // Cookie: `token=${token}; KakaoToken=${ktoken}; `
+    //                 Cookie: req.headers.cookie, // 현재 쿠키를 그대로 전달
+    //             },
+    //         });
 
-            if (Response.data === '토큰 인증 성공' || Response.data === '카카오 토큰 인증 성공') { //인증 성공일 때 댓글 삭제 가능
-                await conn.query(delQuery, [EpId, UsId, comID]); //episodeID, userID, comID 입력 후 댓글 삭제
-                res.send('댓글이 삭제되었습니다.');  //응답
-            } else { //토큰 인증 실패했을 때 
-                res.status(401).send('로그인 하세요');
-            }
-        } catch (error) {
-            // console.error(error);
-            res.status(500).json('입력 실패');
-        } finally {
-            conn.release();
-        }
-    })
+    //         if (Response.data === '토큰 인증 성공' || Response.data === '카카오 토큰 인증 성공') { //인증 성공일 때 댓글 삭제 가능
+    //             await conn.query(delQuery, [EpId, UsId, comID]); //episodeID, userID, comID 입력 후 댓글 삭제
+    //             res.send('댓글이 삭제되었습니다.');  //응답
+    //         } else { //토큰 인증 실패했을 때 
+    //             res.status(401).send('로그인 하세요');
+    //         }
+    //     } catch (error) {
+    //         // console.error(error);
+    //         res.status(500).json('입력 실패');
+    //     } finally {
+    //         conn.release();
+    //     }
+    // })
     
 }
 module.exports = commentAPI;
