da4fed30ef339ebbd0626b48dcbca67d016bd39c
리진아 <mnb2098@naver.com>
2023-08-11 17:00:12 +0900

    머지시키기

--- server/apiCode/webtoonDelete.js
+++ server/apiCode/webtoonDelete.js
@@ -8,10 +8,9 @@ const webtoonDeleteApi = (server, getConn) => {
     server.del('/api/webtoonDelete', async (req, res) => {
         const conn = await getConn();
         const { ID } = req.body;
+        const webtoonQuery = 'CALL usp_delete_webtoon(?);';
 
         try {
-            const webtoonQuery = 'CALL usp_delete_webtoon(?);';
-        
             const [week] = await conn.query(webtoonQuery, ID); 
             res.send("삭제 성공");
 
