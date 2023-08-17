ad50c1ce2f9ac9e06472690639eb30977b960924
리진아 <mnb2098@naver.com>
2023-08-17 14:51:46 +0900

    ㅡ

--- README.md
+++
@@ -1,4 +0,0 @@
-# WebtoonSite
-avatye_웹툰프로젝트
-: 김솔빈, 이진아
-0703 start ..
--- server/routes/commentRoute.js
+++
@@ -1,8 +0,0 @@
-const CommentController = require('../controllers/commentController');
-
-module.exports = (server) => {
-    // 댓글 조회
-    server.get('/api/comment', CommentController.viewComment); //요청이 들어왔을 때 메서드 호출
-    // 댓글 입력
-    server.post('/api/comment_insert', CommentController.insertComment);
-};
