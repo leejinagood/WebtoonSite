d48861c7ca3dcb84928e3d95b926ae747e922c51
리진아 <mnb2098@naver.com>
2023-08-21 11:51:30 +0900

    경로 수정

--- my-app/src/OAuth.js
+++ my-app/src/OAuth.js
@@ -1,3 +1,3 @@
 export const CLIENT_ID = "c3f27e51572a41e42f4204019b897192";
-export const REDIRECT_URL = "http://107.23.243.5/:4000/api/Kakao";
+export const REDIRECT_URL = "http://107.23.243.5:4000/api/Kakao";
 export const Kakao_Auth_Url = `https://kauth.kakao.com/oauth/authorize?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URL}&response_type=code`;
