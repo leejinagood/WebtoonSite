e0d191581ba2d9ea7151f9b1713a02d67de87fe0
리진아 <mnb2098@naver.com>
2023-08-22 11:59:15 +0900

    리다이렉트 코드

--- my-app/src/OAuth.js
+++ my-app/src/OAuth.js
@@ -1,3 +1,3 @@
 export const CLIENT_ID = "c3f27e51572a41e42f4204019b897192";
-export const REDIRECT_URL = "https://main.d9cidza1ul6q9.amplifyapp.com/";
+export const REDIRECT_URL = "http://localhost:3000";
 export const Kakao_Auth_Url = `https://kauth.kakao.com/oauth/authorize?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URL}&response_type=code`;
