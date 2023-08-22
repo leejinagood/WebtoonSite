45ec14e48b5c7818ce5fae195c3e4f4ba676864f
리진아 <mnb2098@naver.com>
2023-08-22 16:20:14 +0900

    인증 받는 코드 추가

--- my-app/src/pages/api/likeApi.js
+++ my-app/src/pages/api/likeApi.js
@@ -5,10 +5,12 @@ export default async function handler(req, res) {
   
   if (UserEmail) {
     try {
-      const response = await axios.get(`http://3.39.187.19:4000/api/update_like`,({
+      const response = await axios.get(`http://3.39.187.19:4000/api/update_like`,{
+        params:{
         EnName: EnName,
-        UserEmail: UserEmail,
-      }));
+        UserEmail: UserEmail},
+        withCredentials: true
+      });
       const Like = response.data;
       res.status(200).json(Like);
     } catch (error) {
