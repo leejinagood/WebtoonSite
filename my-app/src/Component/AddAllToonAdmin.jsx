a089b3ef3cde5ecabf2a8c953788ae5c3e14aea6
solbin <126735005+solbinsol@users.noreply.github.com>
2023-08-24 10:45:36 +0900

    ss

--- my-app/src/Component/AddAllToonAdmin.jsx
+++ my-app/src/Component/AddAllToonAdmin.jsx
@@ -54,7 +54,7 @@ const addAllToonAdmin = () => {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await fetch("/api/adminWebtoon");
+        const response = await fetch("http://3.39.187.19:4000/api/adminWebtoon");
         const data = await response.json();
         setWebtoons(data);
       } catch (error) {
@@ -75,7 +75,7 @@ const addAllToonAdmin = () => {
     if (WebtoonDelete === "삭제"){
       try {
       // 삭제 요청 보내는 로직 추가 (axios 또는 fetch 사용)
-      const response = await fetch("/api/webtoonDelete", {
+      const response = await fetch("http://3.39.187.19:4000//api/webtoonDelete", {
         method: "DELETE",
         headers: {
           "Content-Type": "application/json",
